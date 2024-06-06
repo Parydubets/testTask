4e5a04515fe790edac8a6cbac5e4366873ce5241
Bohdan Parydubets <bogdanparidubec@gmail.com>
2024-06-06 12:48:42 +0300

    Edited django.sh

--- django.sh
+++ django.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 echo "Creating Migrations..."
-python manage.py makemigrations djangoapp
+python manage.py makemigrations app
 echo ====================================
 
 echo "Starting Migrations..."
-python manage.py migrate
+python manage.py migrate app
 echo ====================================
 
 python manage.py populate
 
-pytest testTask
+pytest --create-db
 
 echo "Starting Server..."
 python manage.py runserver 0.0.0.0:8000
